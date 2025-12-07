a2d80f4141053b0fd780baafa8be988ed1c603c1
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2025-12-07 23:46:39 +0100

    TLB-NI - Init login page

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+type LoginPageProps = {
+  params: Promise<void>
+}
+
+export default function LoginPage({}: LoginPageProps) {
+  return (
+    <main className="">
+      <p>LoginPage</p>
+    </main>
+  )
+}
