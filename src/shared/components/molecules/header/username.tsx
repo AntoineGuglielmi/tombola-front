dfc3a393c2d570dea71202ea147a32ce123f4b6d
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2025-12-15 23:58:10 +0100

    TLB-NI - Handle after login

---
+++ src/shared/components/molecules/header/username.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { cn } from '@/shared/lib/utils'
+import { UseUser } from '@/shared/store/UseUser'
+import { Skeleton } from '../../ui/skeleton'
+
+type UsernameProps = object
+
+export default function Username({}: UsernameProps) {
+  const user = UseUser((s) => s.user)
+
+  if (!user) {
+    return <Skeleton className="h-6 w-32 my-0.5" />
+  }
+
+  return <p className="fw-700 fz-5">{user?.username}</p>
+}
